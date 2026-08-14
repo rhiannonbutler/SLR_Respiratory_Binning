8cd3d14076f16aa2090b55239b60398f2aa02a54
Rhiannon Butler <rhiannonbutler20@gmail.com>
2026-08-14 12:53:27 -0400

    Fix variable name bug

--- Example_Recon.py
+++ Example_Recon.py
@@ -254,7 +254,7 @@ for slc in slices_to_process:
     # tmp = np.angle(tmp)
 
     # get k-means cluster indices, with nbins clusters
-    idx = sklearn.cluster.KMeans(n_clusters=nbins, random_state=42).fit(
+    idx_kmeans = sklearn.cluster.KMeans(n_clusters=nbins, random_state=42).fit(
         tmp.reshape((-1, tmp.shape[-1]))).labels_.reshape((nrep, -1))
     print("shuffling k-means cluster indices")
     idx_flat = idx_kmeans.ravel()
